5cb7370ad294801fee32baec73a5a062e7b40ed4
fahad <fahad.itwork@gmail.com>
2022-11-19 22:36:34 +0600

    modified:   .idea/C 03. Flutter.iml 	new file:   .idea/libraries/Dart_Packages.xml 	new file:   .idea/libraries/Dart_SDK.xml 	modified:   00. flutter_default/lib/main.dart

--- 00. flutter_default/lib/main.dart
+++ 00. flutter_default/lib/main.dart
@@ -23,20 +23,29 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
+  MySnakeBar(message,context){
+
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Project_1"),
-        titleSpacing: 0,
+        titleSpacing: 5,
         centerTitle: false,
         toolbarHeight: 60,
         toolbarOpacity: 1,
-        elevation: 10,
+        elevation: 8,
         backgroundColor: Colors.indigo,
-        actions: [ on
-
+        actions: [ 
+          IconButton(onPressed: (){MySnakeBar("comments", context);}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){MySnakeBar("Book now", context);}, icon: Icon(Icons.add_call)),
+          IconButton(onPressed: (){MySnakeBar("24/7 Support", context);}, icon: Icon(Icons.record_voice_over)),
         ],
 
       )
@@ -45,3 +54,4 @@ class MyHomePage extends StatelessWidget {
   }
 
 }
+
