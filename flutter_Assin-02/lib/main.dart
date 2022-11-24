023dded2432604a07964ada0844238efd0b74e08
fahad <fahad.itwork@gmail.com>
2022-11-24 13:24:39 +0600

    modified:   flutter_Assin-02/lib/main.dart
    	modified:   flutter_Assin-02/lib/main.dart

--- flutter_Assin-02/lib/main.dart
+++ flutter_Assin-02/lib/main.dart
@@ -1,115 +1,109 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData(primarySwatch: Colors.grey),
+      color: Colors.white10,
+      home: const MyHomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  MySnackBar(message,context){
+    return ScaffoldMessenger.of (context).showSnackBar(
+      SnackBar(content: Text(message),duration: const Duration(milliseconds: 500),)
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+        // leading: IconButton(onPressed: (){},icon: const Icon(Icons.logo_dev)),
+        title: const Text("Demo_App"),
+        backgroundColor: Colors.blue[700],
+        titleSpacing: 0,
+        elevation: 8,
+        centerTitle: false,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        actions: [IconButton(onPressed: () {MySnackBar("Notifications",context);}, icon: const Icon(Icons.notifications)),
+          IconButton(onPressed: () {MySnackBar("Call us",context);}, icon: const Icon(Icons.call)),
+          IconButton(onPressed: () {MySnackBar("Profile",context);}, icon: const Icon(Icons.account_circle)),
+        ]),
+
+
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        elevation: 8,
+        onPressed:(){MySnackBar("Massage",context);},
+        backgroundColor: Colors.pinkAccent,
+        child: const Icon(Icons.message),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex : 0,
+        iconSize : 24,
+        backgroundColor : Colors.white60,
+        fixedColor : Colors.blueGrey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.schedule),label: "Schedule" ),
+          BottomNavigationBarItem(icon: Icon(Icons.menu_book),label: "Library"),
+          // BottomNavigationBarItem(icon: Icon(Icons.menu_book_sharp),label: "Library") //Why 4th icon cause problem??
+        ],
+        onTap: (int index){
+          if (index==0){MySnackBar("Home",context);}
+          if (index==1){MySnackBar("Schedule",context);}
+          if (index==2){MySnackBar("Library",context);}
+        },
+      ),
+
+      drawer: Drawer(
+        child: ListView(
+          children:[
+            DrawerHeader(
+              padding: EdgeInsets.all(2),
+                child: UserAccountsDrawerHeader(
+                  decoration: const BoxDecoration(
+                      color: Colors.cyan,
+                      image: DecorationImage(
+                        image: NetworkImage('https://www.digiedupro.com/media/2020/07/Beijing_Institute_of_Graphic_Communication_Slider_4.jpg'),
+                        fit: BoxFit.cover,
+                  )
+                  ),
+                  accountName: Text("Demo_App"),
+                  accountEmail: Text("fahad.itwork@gmail.com"),
+                  currentAccountPicture: Image.network("https://lh3.googleusercontent.com/p/AF1QipN6O9nyN-k9DZjyqwYCllByMaTTrK-u8yO_1pCb=s680-w680-h510"),
+                )
+            ),
+
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text("Home"),
+              onTap: (){MySnackBar("_button",context);},
+              trailing: const Icon(Icons.add),
+              selected: true,),
+            ListTile(leading: Icon(Icons.notifications),title: Text("Notifications"),onTap: (){MySnackBar("_button",context);}),
+            ListTile(leading: Icon(Icons.schedule),title: Text("Schedule"),onTap: (){MySnackBar("_button",context);}),
+            ListTile(leading: Icon(Icons.menu_book),title: Text("Library"),onTap: (){MySnackBar("_button",context);},enabled: false,
+            trailing: Text("Under work"),),
+            ListTile(leading: Icon(Icons.newspaper),title: Text("Results"),onTap: (){MySnackBar("_button",context);}),
+          ]
+        )
+      ),
     );
   }
 }
+
+
